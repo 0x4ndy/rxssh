c45bb8edfea24f012c2fdcacf06cd0c78defcc67
Andy <andy@andy.codes>
2021-02-15 07:49:26 +0100

    Added initial function to read command line args.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
     let output = Command::new("ls").args(&["."]).status().expect("Unable to execute this process.");
 
     println!("{}", output.to_string());
