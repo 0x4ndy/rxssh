b531bc76e8c4432007e7bf8b1ec02eda2610141b
Andy <andy@andy.codes>
2021-03-08 00:56:03 +0100

    Handled error codes of executed commands.

--- src/modules/ssh/src/lib.rs
+++ src/modules/ssh/src/lib.rs
@@ -25,18 +25,64 @@ pub fn execute_single(
     channel.read_to_string(&mut output)?;
 
     match channel.exit_status() {
-        Ok(code) => exit_code_to_string(code, &mut output),
+        Ok(code) => exit_code_to_string(code, &mut output, command),
         Err(err) => return Err(err.into()),
     };
 
     Ok(output)
 }
 
-fn exit_code_to_string(code: i32, output: &mut String) {
-    if code != 0 {
-        output.clear();
-        // TODO: Handle this by a switch on different code
-        //       values and return a string interpretation
-        output.push_str(&code.to_string());
+fn exit_code_to_string(code: i32, output: &mut String, command: &str) {
+    match code {
+        0 => {
+            // All good here
+        }
+        1 => {
+            output.push_str(
+                &(format!(
+                    "{}: exit with code {}; catchall for general errors.",
+                    command, code
+                )),
+            );
+        }
+        2 => {
+            output.push_str(
+                &(format!(
+                    "{}: exit with code {}; misuse of shell built-ins.",
+                    command, code
+                )),
+            );
+        }
+        126 => {
+            output.push_str(
+                &(format!(
+                    "{}: exit with code {}; command invoked cannot execute.",
+                    command, code
+                )),
+            );
+        }
+        127 => {
+            output.push_str(&(format!("{}: exit with code {}; command not found.", command, code)));
+        }
+        128 => {
+            output.push_str(
+                &(format!(
+                    "{}: exit with code {}; invalid argument to exit.",
+                    command, code
+                )),
+            );
+        }
+        129..=255 => {
+            let signal = code - 128;
+            output.push_str(
+                &(format!(
+                    "{}: exit with code {}; fatal error signal {}.",
+                    command, code, signal
+                )),
+            );
+        }
+        _ => {
+            output.push_str(&(format!("{}: exit with code {}; Unknown error.", command, code)));
+        }
     }
 }
